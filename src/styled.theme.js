fb4d53f631549830993561bc23a7b69390965409
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-19 02:44:43 -0500

    Se ha creado el theme general para el proyecto

---
+++ src/styled.theme.js
@@ -0,0 +1,22 @@
+// example theme.js
+export default {
+  breakpoints: ['22.50em', '48.750em', '64em'],
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64
+  ],
+  colors: {
+    main: '#FF8721',
+    light: '#B0B0B0',
+    dark: '#636363'
+  },
+  space: [
+    0, 4, 8, 16, 32, 64, 128, 256
+  ],
+  fonts: {
+    sans: 'Roboto, sans-serif',
+  },
+  shadows: {
+    small: '0 0 4px rgba(0, 0, 0, .125)',
+    large: '0 0 24px rgba(0, 0, 0, .125)'
+  }
+}
